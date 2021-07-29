77af29f6e2c267cfebb42f1d9b1151cc93d405ac
Oesparza <omar.esparza@ventacero.mx>
2021-07-29 10:03:08 -0500

    [ADD] Estate: Add new module to repo

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021, OmarEsparza
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Estate',
+    'summary': 'Module summary',
+    'version': '13.0.1.0.0',
+    'category': 'Real State',
+    'website': 'https://www.jarsa.com.mx/',
+    'author': 'OmarEsparza',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'base',
+    ],
+    'data': [],
+    
+}
